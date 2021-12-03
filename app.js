3a37d017b76335b394f1c97d536b67a22762a03f
avihavai <hellasa1@t31300-lr069.org.aalto.fi>
2021-12-03 15:18:46 +0200

    Update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import { Application } from "https://deno.land/x/oak@v9.0.1/mod.ts";
 const app = new Application();
 
 app.use(({response}) => {
-    response.body = "Hei maailma! Muuttuuko tämä?";
+    response.body = "Hei maailma!";
 });
 
 export { app };
